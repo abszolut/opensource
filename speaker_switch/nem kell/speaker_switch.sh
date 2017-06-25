e868789c9002fa7de43a17214ec78c0bfc85c7b2
abszolut <abszolut@github.com>
2017-06-25 14:31:27 +0200

    deleted

--- speaker_switch/nem kell/speaker_switch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "on" > /home/abszolut/app/speaker_switch/speaker.log
-echo "curl -X POST  -H \"Content-Type: application/json\" -d \"{\\\"value1\\\":\\\"on\\\"}\" https://maker.ifttt.com/trigger/speaker_on/with/key/neVVqg3UhlzDVPWArtzj_pA_ZM0tXWLmmZHry0G7BgT" > /home/abszolut/app/speaker_switch/speaker.sh
-
-
-
